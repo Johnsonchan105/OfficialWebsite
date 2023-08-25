b4c32dd71ff38a5a5cf6030c3e5d4060e03691a5
Johnson Chan <44689240+Johnsonchan105@users.noreply.github.com>
2023-08-24 19:52:15 -0700

    completing html portion

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class = 'App'>
+      <div id='content'>
+        <div id = 'intro'>
+          <div id='intro_thing'></div>
+          <div class = 'Title'>
+            <h1>Hi! I'm Johnson.</h1>
+          </div>
+          <div class='fade-in-section is-visible'>
+            <div class='intro-desc'>
+              I'm a software engineer based in Campbell, CA, who loves crafting frontends, 
+              exploring machine learning, enhancing the way people interact with technology, 
+              and everything in between.
+            </div>
+          </div>
+        </div>
+        <div id = 'about'>
+          <div class='fade-in-section is-visible'>
+            <div class = 'section-header'></div>
+            <div class = 'about-content'>
+              <p>
+                Hi! My name is Cheng Han, but I go by Johnson! I am currently undertaking a 
+                Bachelor's of Sceince in Computer Science at the <b>University of California, Santa Barbara</b>
+                and will be completing the degree in June 2024. Current I am also taking an internship at <b> YSTEM and Chess</b>
+                while performing as an Undergraduate Learning Assistant at UCSB.
+              </p>
+              I have been working with these technologies!
+              <ul class = 'tech-stack'>
+                <div>
+                  <li>Python</li>
+                </div>
+                <div>
+                  <li>Javascript</li>
+                </div>
+                <div>
+                  <li>HTML&CSS</li>
+                </div>
+                <div>
+                  <li>Node.js</li>
+                </div>
+                <div>
+                  <li>React.js</li>
+                </div>
+                <div>
+                  <li>Angular.js</li>
+                </div>
+              </ul>
+              <p>Outside of work you’ll probably find me rock climbing, playing video games (Riot Games ily &lt;3), or obsessing over my latest hyperfixation.</p>
+            </div>
+          </div>
+        </div>
+        <div id = 'experience'>
+          <div class='fade-in-section is-visible'>
+            <div class = 'section-header'></div>
+            
+          </div>
+        </div>
+        <div id = 'projects'>
+          <div class='fade-in-section is-visible'>
+
+          </div>
+        </div>
+        <div class = 'fade-in-section'>
+          <div id='footer'>
+            <div class='credits'>
+                <div>Designed in Figma and Built by Yours Truly.</div>
+                <div>All Rights Reserved. ©</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class = 'navbar'></div>
     </div>
   );
 }
