7bd5a93d252c08df13e3a15d1d4a646a5a0898d9
Johnson Chan <44689240+Johnsonchan105@users.noreply.github.com>
2023-08-28 16:39:09 -0700

    import material-ui

--- my-app/src/components/ExperienceList.js
+++ my-app/src/components/ExperienceList.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import PropTypes from "prop-types";
+import { makeStyles } from "@material-ui/core/styles";
 import FadeInSection from "./FadeInSection";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import { Typography } from '@material-ui/core/Typography';
+import { Box } from '@material-ui/core/Box';
+
 
 const ExperienceList = () => {
     //const classes = useStyles();
