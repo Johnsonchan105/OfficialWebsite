574ee14e05034383149d96c1d83ffa0e6f36340e
Johnson Chan <44689240+Johnsonchan105@users.noreply.github.com>
2023-09-09 22:09:16 -0700

    resume page + other

---
+++ my-app/src/components/Resume.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Resumepdf from "../assets/resume.pdf";
+
+class Resume extends React.Component {
+    render() {
+        return (
+            <div id='resume'>
+                <div className='resume-title'>
+                    {"Resume"}
+                </div>
+            </div>
+        );
+    }
+}
